e5f25f00fb49d8e42d1cf4215ac8e065240cf996
Smriti Naik <smritinaik1421@gmail.com>
2021-02-21 23:59:41 +0530

    prepare for deploy

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
+import ContactState from './context/contact/contactState';
+import AuthState from './context/auth/authState'
+import AlertState from './context/alert/AlertState'
+
+
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
+import setAuthToken from './utils/setAuthToken';
+
+import PrivateRoute from './components/routing/PrivateRoute';
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
+
+
+const App = () => {
+  return (
+    <AuthState>
+    <ContactState>
+      <AlertState>
+    <Router>
+    <Fragment>
+      <Navbar />
+       <div className="container">
+          <Alert />
+          <Switch>
+            <PrivateRoute exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+       </div>
+    </Fragment>
+    </Router>
+    </AlertState>
+    </ContactState>
+    </AuthState>
+  );
+}
+
+export default App;
